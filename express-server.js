294efa86f0bcee2b11d394c524c57921fb384147
Negar Amiri <negamiri@gmail.com>
2018-12-06 00:17:34 +0000

    Login working and username is being stored in username being kept in cookie

--- express-server.js
+++ express-server.js
@@ -8,12 +8,14 @@ var cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//Homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,63 +24,71 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//READ list of all our URLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase}
   res.render("urls-index", templateVars);
 });
 
-//Form for creating short URL
+//READ form for creating new URLs
 app.get("/urls/new", (req, res) => {
   res.render("urls-new");
 });
 
-//After filling in form:
-//Adds long and short URL to URLdatabase
+//CREATE login
+app.post('/urls/login', (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
+  res.redirect(302, "/urls/");
+});
+
+//CREATE short URL
 //Redirects to /urls/{shortURL} page to show long and short url
 app.post("/urls", (req, res) => {
   let longURL = req.body.longURL;
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
-  res.redirect(301, `/urls/${shortURL}`);
+  res.redirect(302, `/urls/${shortURL}`);
 });
 
-//Takes short URL and redirects to long URL
+//READ short URL and redirect to long URL
 app.get("/u/:shortURL", (req, res) => {
   let shorturl = req.params.shortURL;
   let longURL = urlDatabase[shorturl];
   if (shortURL) {
-    res.redirect(301, longURL);
+    res.redirect(302, longURL);
   } else {
     res.status('400');
     res.render('notfound');
   }
 });
 
+//READ page of short URL
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                         longURL: urlDatabase[req.params.id]};
   res.render("urls-show", templateVars);
 });
 
-//Update
+//UPDATE existing URL
 app.post("/urls/:id/update", (req, res) => {
   let shorturl = req.params.id;
   let longurl = req.body.longURL;
   urlDatabase[shorturl] = longurl;
-  res.redirect(301, "/urls");
+  res.redirect(302, "/urls");
 });
 
-//Delete
+//DELETE existing long url
 app.post("/urls/:id/delete", (req, res) => {
   let shorturl = req.params.id;
   delete urlDatabase[shorturl];
-  res.redirect(301, "/urls");
+  res.redirect(302, "/urls");
 });
 
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 //To ensure server is running
 app.listen(port, () => {
