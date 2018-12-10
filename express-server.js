894d8c5070ddc320e4fb11794ace4dd4960c8d2b
Negar Amiri <negamiri@gmail.com>
2018-12-10 00:35:33 +0000

    Fixed issue with login where you would get error message saying 'cannot convert undefined or null to object'

--- express-server.js
+++ express-server.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Middleware
 const express = require("express");
 const app = express();
 const port = 8080; // default port 8080
@@ -49,171 +50,6 @@ const users = {
     password: "test2343"
   }
 };
-
-//Homepage
-app.get("/", (req, res) => {
-  res.redirect("/login");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-//REGISTER AND LOGIN
-
-//Login
-app.get("/login", (req, res) => {
-  let templateVars = {
-    user: users[req.session.userid],
-  }
-  if (templateVars.user){
-    res.redirect("/urls");
-  } else{
-    res.render("login", templateVars);
-  }
-})
-
-//Create login
-app.post('/login', (req, res) => {
-  if (req.body["email"] == "" || req.body["password"] == ""){
-    res.status('400');
-    res.send("Please provide an email and password to login")
-  } else if (checkLogin(req)) {
-    req.session.userid = grabId(req.body.email);
-    delete res.session.error;
-    res.redirect("/urls/");
-  } else {
-    res.send("Incorrect password or email entered.")
-  }
-});
-
-//Log out
-app.post('/logout', (req, res) => {
-  req.session = null;
-  res.redirect(302, "/urls/");
-});
-
-//Registration page
-app.get("/register", (req, res) => {
-  let templateVars = {
-    user: users[req.session.userid]
-  }
-  if (templateVars.user){
-    res.redirect("/urls")
-  } else {
-    res.render("registration", templateVars);
-  }
-});
-
-//Register user in database
-app.post("/register", (req, res) => {
-  if (req.body.email == "" || req.body.password == ""){
-    res.status('400');
-    res.send("Please provide an email and password to register")
-  } else if (emailTaken(req)) {
-      res.status('400');
-      res.send("Emaill already in use")
-  } else {
-      let id = generateRandomString();
-      users[id] = {
-        "id": id,
-        "email": req.body.email,
-        "password": bcrypt.hashSync(req.body.password, 10),
-      }
-      req.session.userid = users[id].id;
-      res.redirect("/urls/");
-    }
-});
-
-
-//List of all of user's URLs
-app.get("/urls", (req, res) => {
-  const templateVars = {
-      urls: urlsForUser(req.session.userid),
-      user: users[req.session.userid]
-  };
-  if (templateVars.user) {
-    res.render("urls-index", templateVars);
-  } else {
-    res.redirect("/login?e=" + encodeURIComponent('Log in to continue'));
-  }
-});
-
-//Page for creating new URL
-app.get("/urls/new", (req, res) => {
-  let templateVars = { user: users[req.session.userid] }
-  if (templateVars.user) {
-    res.render("urls-new", templateVars);
-  } else {
-    res.redirect("/login");
-  }
-});
-
-
-//CREATE short URL
-//Redirects to /urls/{shortURL} page to show long and short url
-app.post("/urls", (req, res) => {
-  let longURL = req.body.longURL;
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = {
-    longurl: longURL,
-    userid: req.session.userid
-  };
-  res.redirect(`/urls/${shortURL}`);
-});
-
-//READ short URL and redirect to long URL
-app.get("/u/:shortURL", (req, res) => {
-  let shorturl = req.params.shortURL;
-  let longURL = (urlDatabase[shorturl] || {longurl: '/notfound' }).longurl;
-  if (shorturl) {
-    res.redirect(longURL);
-  } else {
-    res.status("404").send('Not found');
-  }
-});
-
-app.get("/notfound", (req,res) => {
-  res.status("400").send("Long URL not found");
-})
-
-//View short url
-app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    longURL: (urlDatabase[req.params.id] || {longurl: '/notfound' }).longurl,
-    user: users[req.session.userid]
-  };
-  if (req.session.userid){
-    if (userURL(req)) {
-      res.render("urls-show", templateVars);
-    } else {
-      res.send("You do not own this short URL.");
-    }
-  } else {
-    res.status("404").send("Please log in to continue");
-  }
-});
-
-//UPDATE existing URL
-app.post("/urls/:id/update", (req, res) => {
-  let shorturl = req.params.id;
-  urlDatabase[shorturl].longurl = req.body.longURL;
-  res.redirect("/urls");
-});
-
-//DELETE existing long url
-app.post("/urls/:id/delete", (req, res) => {
-  let shorturl = req.params.id;
-  delete urlDatabase[shorturl];
-  res.redirect("/urls");
-});
-
-
-//To ensure server is running
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
 
 //Create a random string for short url
 //Limitations: could be a duplicate as we're not checking for existing values
@@ -293,3 +129,169 @@ function checkShorturl(short) {
   }
   return false;
 }
+
+//Homepage
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+//REGISTER AND LOGIN
+
+//Login
+app.get("/login", (req, res) => {
+  let templateVars = {
+    user: users[req.session.userid],
+  }
+  if (templateVars.user){
+    res.redirect("/urls");
+  } else{
+    res.render("login", templateVars);
+  }
+})
+
+//Create login
+app.post('/login', (req, res) => {
+  if (req.body["email"] == "" || req.body["password"] == ""){
+    res.status('400');
+    res.send("Please provide an email and password to login")
+  } else if (checkLogin(req)) {
+    req.session.userid = grabId(req.body.email);
+    res.redirect("/urls/");
+  } else {
+    res.send("Incorrect password or email entered.")
+  }
+});
+
+//Log out
+app.post('/logout', (req, res) => {
+  req.session = null;
+  res.redirect(302, "/urls/");
+});
+
+//Registration page
+app.get("/register", (req, res) => {
+  let templateVars = {
+    user: users[req.session.userid]
+  }
+  if (templateVars.user){
+    res.redirect("/urls")
+  } else {
+    res.render("registration", templateVars);
+  }
+});
+
+//Register user in database
+app.post("/register", (req, res) => {
+  if (req.body.email == "" || req.body.password == ""){
+    res.status('400');
+    res.send("Please provide an email and password to register")
+  } else if (emailTaken(req)) {
+      res.status('400');
+      res.send("Emaill already in use")
+  } else {
+      let id = generateRandomString();
+      users[id] = {
+        "id": id,
+        "email": req.body.email,
+        "password": bcrypt.hashSync(req.body.password, 10),
+      }
+      req.session.userid = users[id].id;
+      res.redirect("/urls/");
+    }
+});
+
+
+//List of all of user's URLs
+app.get("/urls", (req, res) => {
+  const templateVars = {
+      urls: urlsForUser(req.session.userid),
+      user: users[req.session.userid]
+  };
+  if (templateVars.user) {
+    res.render("urls-index", templateVars);
+  } else {
+    res.redirect("/login?e=" + encodeURIComponent("Log in to continue"));
+  }
+});
+
+//Page for creating new URL
+app.get("/urls/new", (req, res) => {
+  let templateVars = { user: users[req.session.userid] }
+  if (templateVars.user) {
+    res.render("urls-new", templateVars);
+  } else {
+    res.redirect("/login?e=" + encodeURIComponent("Log in to continue"));
+  }
+});
+
+
+//CREATE short URL
+//Redirects to /urls/{shortURL} page to show long and short url
+app.post("/urls", (req, res) => {
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    longurl: longURL,
+    userid: req.session.userid
+  };
+  res.redirect(`/urls/${shortURL}`);
+});
+
+//READ short URL and redirect to long URL
+app.get("/u/:shortURL", (req, res) => {
+  let shorturl = req.params.shortURL;
+  let longURL = (urlDatabase[shorturl] || {longurl: '/notfound' }).longurl;
+  if (shorturl) {
+    res.redirect(longURL);
+  } else {
+    res.status("404").send('Not found');
+  }
+});
+
+app.get("/notfound", (req,res) => {
+  res.status("400").send("Long URL not found");
+})
+
+//View short url
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: (urlDatabase[req.params.id] || {longurl: '/notfound' }).longurl,
+    user: users[req.session.userid]
+  };
+  if (req.session.userid){
+    if (userURL(req)) {
+      res.render("urls-show", templateVars);
+    } else {
+      res.send("You do not own this short URL.");
+    }
+  } else {
+    res.status("404").send("Please log in to continue");
+  }
+});
+
+//UPDATE existing URL
+app.post("/urls/:id/update", (req, res) => {
+  let shorturl = req.params.id;
+  urlDatabase[shorturl].longurl = req.body.longURL;
+  res.redirect("/urls");
+});
+
+//DELETE existing long url
+app.post("/urls/:id/delete", (req, res) => {
+  let shorturl = req.params.id;
+  delete urlDatabase[shorturl];
+  res.redirect("/urls");
+});
+
+
+//To ensure server is running
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+
